e804ddce5f2e37273dae7fccb554b216976af14d
Vi0let-0wner <168981926+Vi0let-0wner@users.noreply.github.com>
2024-05-10 21:19:33 +0700

    ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,8 @@
 module.exports = {
-    name: 'ping',
-    description: 'Ping!',
+    data: {
+        name: 'ping',
+        description: 'Ping!',
+    },
     execute: (message, args) => {
         message.channel.send('Pinging...').then(sent => {
             sent.edit(`My! Latency is ${sent.createdTimestamp - message.createdTimestamp}ms.`);
